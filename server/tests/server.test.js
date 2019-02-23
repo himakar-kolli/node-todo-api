da45d332680562eb6b9e4a374dbebf0559c2f433
Himakar <KOLLIHX@legal.regn.net>
2019-02-23 13:00:46 -0500

    Test GET /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,8 @@
 const expect = require('expect');
 const request = require('supertest');
+const {
+  ObjectID
+} = require('mongodb');
 
 const {
   app
@@ -9,14 +12,17 @@ const {
 } = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
-// beforeEach() is a Mocha 'hook' that runs before executing each test case (each it cond'n) in this file, used to set up pre-conditions..
+/* beforeEach() is a Mocha 'hook' that runs before executing each test case (each it() cond'n) in this file, 
+   this hook is used to set up pre-conditions.. */
 beforeEach((done) => {
-  Todo.remove({}).then(() => {
+  Todo.deleteMany({}).then(() => {
     return Todo.insertMany(todos);
   }).then(() => done());
 });
@@ -75,6 +81,34 @@ describe('GET /todos', () => {
       .expect((res) => {
         expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
       .end(done);
   });
 });
